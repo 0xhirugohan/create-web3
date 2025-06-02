199a4e5b8bbed8960d616a60dab597d1aee6a829
han <han@fbrns.co>
2025-06-02 18:05:08 +0700

    feat: add static homepage

---
+++ apps/webapp/src/components/Navbar.tsx
@@ -0,0 +1,37 @@
+import { Link } from 'react-router'
+
+import ReactLogo from '../assets/react.svg'
+
+const Navbar = () => {
+    return <div className="bg-zinc-300/95 fixed top-0 left-0 right-0 z-50 p-3 flex justify-between">
+        <div className="flex gap-x-8 items-center">
+            <div className="flex gap-x-2">
+                <img
+                    className="w-8"
+                    src={ReactLogo}
+                    alt="react logo"
+                />
+                <div>
+                    <p className="font-bold">Create-Web3</p>
+                    <p className="text-xs">EVM boilerplate</p>
+                </div>
+            </div>
+            <Link
+                className="hidden md:block cursor-pointer"
+                to="/"
+            >
+                Home
+            </Link>
+        </div>
+        <div className="flex items-center">
+            <button
+                className="text-sm border-2 border-zinc-600 rounded-md px-4 py-2 hover:shadow-md cursor-pointer"
+            >
+                <span className="hidden md:block">Connect Wallet</span>
+                <span className="block md:hidden">Connect</span>
+            </button>
+        </div>
+    </div>
+}
+
+export default Navbar;
--- apps/webapp/src/main.tsx
+++ apps/webapp/src/main.tsx
@@ -1,10 +1,37 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router'
 import './index.css'
-import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import {
+  HomePage,
+  NotFoundPage,
+} from './pages'
+import Layout from './layout'
+
+const IS_PROD = import.meta.env.PROD;
+
+const Content = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+if (IS_PROD) {
+  createRoot(document.getElementById('root')!).render(
+    <Content />,
+  )
+} else {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <Content />
+    </StrictMode>,
+  )
+}
