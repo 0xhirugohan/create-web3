44cd9de0eb466fd797cd8ba47941ae4f6c79def5
han <han@fbrns.co>
2025-06-04 18:18:27 +0700

    feat(contract): setup scripts for deployment

--- apps/contract/script/Counter.s.sol
+++ apps/contract/script/Counter.s.sol
@@ -10,10 +10,12 @@ contract CounterScript is Script {
     function setUp() public {}
 
     function run() public {
+        // uncomment line below if you want to deploy to anvil (local chain)
+        // vm.createSelectFork("anvil");
         vm.startBroadcast();
-
         counter = new Counter();
-
         vm.stopBroadcast();
+
+        // you can deploy multichain on a single script by copying lines above and change the chain
     }
 }
